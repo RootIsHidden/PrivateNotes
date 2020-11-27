6e83bde8f833c9ca7143fdb93370c5dfab542b02
RootIsHidden <root.ishidden@gmail.com>
2020-11-26 20:29:48 -0500

    Added Bootstrap

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
 
+# Default page:
 @app.route('/')
-def hello_world():
-    return 'Wellcome to Private Notes!'
+def index():
+    return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
