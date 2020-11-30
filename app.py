8160adc45c0e22fbdbbb75163accb4a1778f6300
RootIsHidden <root.ishidden@gmail.com>
2020-11-30 06:08:21 -0500

    Added: Note ID/DATE/TITLE/CATEGORY, Tables, Sorting, Privacy

--- app.py
+++ app.py
@@ -10,7 +10,7 @@
 # permission to rely on his videos and implementation for educational purposes.
 # ===============================================================================================================
 
-from flask import Flask, render_template, flash, send_from_directory
+from flask import Flask, render_template, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
@@ -18,6 +18,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_security import Security, SQLAlchemyUserDatastore, login_required, UserMixin, RoleMixin
 from flask_login import current_user
+from datetime import datetime
 import os
 import psycopg2
 import wtf_helpers
@@ -48,7 +49,7 @@ class User(db.Model, UserMixin):
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
-    # Adding note-user relationship:
+    # Adding note-user (aka many to one) relationship:
     notes = db.relationship('Note', backref='user', lazy='dynamic')
 
 # Role class used for access-control:
@@ -59,17 +60,24 @@ class Role(db.Model, RoleMixin):
 
 # Form class
 class NoteForm(Form):
-    # Text field:
-    str_field  = StringField('Note', validators=[DataRequired()])
+    # Note's title:
+    title_str_field     = StringField('Title', validators=[DataRequired()])
+    # Note's category:
+    category_str_field  = StringField('Category')
+    # Note's content:
+    str_field           = StringField('Note', validators=[DataRequired()])
     # Add note button:
     submit_Button = SubmitField('Add Note')
 
 # Note class
 class Note(db.Model):
-    id      = db.Column(db.Integer, primary_key = True)
-    text    = db.Column(db.String(256), unique = False, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
+    id         = db.Column(db.Integer, primary_key = True)
+    user_id    = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date       = db.Column(db.String(128), unique = False, nullable = False)
+    title      = db.Column(db.String(256), unique = False, nullable = False)
+    category   = db.Column(db.String(256), unique = False, nullable = False)
+    text       = db.Column(db.String(512), unique = False, nullable = False)
+
     # Modyfying the constructor:
     def __init__(self, note, user):
         self.text    = note
@@ -91,9 +99,13 @@ def index():
     return render_template('index.html', users=users)
 
 # Default page:
-@app.route('/notes/<user_email>', methods = ['GET', 'POST'])
-def notes(user_email):
+@app.route('/notes/<user_email>/<sorting_mode>', methods = ['GET', 'POST'])
+@app.route('/notes/<user_email>', defaults={'sorting_mode' : 'Date'}, methods = ['GET', 'POST'])
+def notes(user_email, sorting_mode):
     user = User.query.filter_by(email=user_email).first_or_404()
+
+    if user != current_user:
+        abort(403)
 
     # Checking whether user is logged in: 
     if user == current_user:
@@ -103,14 +115,30 @@ def notes(user_email):
 
     if form and form.validate_on_submit():
         # Setting up DB object having a user refference:
-        note = Note(form.str_field.data, user)
+        note            = Note(form.str_field.data, user)
+        note.date       = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        note.title      = form.title_str_field.data
+        
+        category  = form.category_str_field.data
+        if category:
+            note.category = category
+        else:
+            note.category = '-'
+
         # Adding a Note to DB:
         db.session.add(note)
         db.session.commit()
     
-    notesList = user.notes
+    if sorting_mode == 'Date':
+        notesList = user.notes.order_by(Note.date)
+    elif sorting_mode == 'Title':
+        notesList = user.notes.order_by(Note.title)
+    elif sorting_mode == 'Category':
+        notesList = user.notes.order_by(Note.category)
+    else:
+        abort(403)
 
-    return render_template('notes.html', form = form, notesList = notesList, user_email=user_email)
+    return render_template('notes.html', form = form, notesList = notesList, user_email=user_email, sorting_mode = sorting_mode)
 
 if __name__ == '__main__':
     app.run(debug = True)
